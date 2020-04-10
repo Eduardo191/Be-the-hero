6edabb655d7faa315e87f3470e950bbddd0e9912
Eduardo <feduardo191@gmail.com>
2020-04-10 13:54:38 -0300

    Finalizando tela de cadastros

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './style.css'
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+            <header>
+            </header>
+        </div>
+    );
+}
