156c4abeb1f1952752f60234b77d66cc1faaea01
Eduardo <feduardo191@gmail.com>
2020-04-10 15:00:04 -0300

    Finalizando tela de profile

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,12 +1,84 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+import { FiPower, FiTrash2 } from 'react-icons/fi'
 import './style.css'
 
 export default function Profile() {
     return (
         <div className="profile-container">
             <header>
+                <img src={logoImg} alt="Be The Hero" />
+                <span>Bem vinda, APAD</span>
+
+                <Link to="/incidents/new" className="button">Cadastrar novo caso</Link>
+                <button type="button">
+                    <FiPower size={18} color="#E02041" />
+                </button>
             </header>
+
+            <h1>Casos cadastrados</h1>
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÂO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>Valor teste</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÂO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>Valor teste</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÂO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>Valor teste</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÂO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>Valor teste</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+            </ul>
         </div>
     );
 }
