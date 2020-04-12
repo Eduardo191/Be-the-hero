7a1de5e7899fdeb445992468535311592452431d
Eduardo <feduardo191@gmail.com>
2020-04-11 21:17:30 -0300

    último commit

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,13 +1,13 @@
 const connection = require('../database/connection')
 
 module.exports = {
-    async index(requset, response) {
+    async index(request, response) {
         const { page = 1 } = request.query
 
         const [count] = await connection('incidents').count()
 
         const incidents = await connection('incidents')
-            .join('ongs', 'ongs_id', '=', 'incidents.ong_id')
+            .join('ongs', 'ong_id', '=', 'incidents.ong_id')
             .limit(5).offset((page - 1) * 5)
             .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf'])
 
